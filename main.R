3b122ff6419b3b1154bc6b80b5e6cc8899e6c8ba
RitikOnTheRoad <learn.ritikmathpal@gmail.com>
2025-12-26 00:48:59 +0530

    till weighted lr

---
+++ main.R
@@ -0,0 +1,97 @@
+rm(list=ls())
+library(dplyr)
+
+data=read.csv("https://raw.githubusercontent.com/RitikOnTheRoad/Statistical-Analysis-of-Stroke-Risk-Factors/refs/heads/main/healthcare-dataset-stroke-data.csv")
+
+data=as_tibble(data)
+summary(data)
+head(data)
+
+
+table(data$work_type)
+table(data$gender)
+table(data$Residence_type)
+table(data$smoking_status)
+table(data$ever_married)
+table(data$Residence_type)
+unique(data$smoking_status)
+
+#unknown bmi values are entered as "N/A" instead of NA
+data$bmi[data$bmi=="N/A"]=NA
+sum(is.na(data))
+
+#removing the gender "other" for model simplicity
+data=data[data$gender!="Other",]
+
+
+data=data %>% 
+  mutate(
+    gender=as.factor(gender),
+    hypertension=as.factor(hypertension),
+    heart_disease=as.factor(heart_disease),
+    ever_married=as.factor(ever_married),
+    Residence_type=as.factor(Residence_type),
+    stroke=as.factor(stroke),
+    bmi=as.double(bmi),
+    smoking_status=replace(smoking_status,smoking_status=="Unknown",NA), #labeling unknown smoking status as NA
+    smoking_status=factor(
+      smoking_status,
+      levels=c('never smoked',"formerly smoked","smokes"),
+      ordered=TRUE
+    ),
+    work_type=as.factor(work_type),
+    age10=floor(age/10),
+    avg_glucose_level=scale(avg_glucose_level),
+    bmi=scale(bmi)
+  ) %>% 
+  select(-id,-age)
+data
+
+#imbalanced class
+
+library(ggplot2)
+ggplot(data,aes(stroke,fill=stroke))+geom_bar()+
+  scale_x_discrete(labels=c("no","yes"))+
+  labs(title="class distribution")
+
+
+#Train test split
+library(rsample)
+
+set.seed(42)
+data_split=initial_split(data,prop = 0.7)
+train_data=training(data_split)
+test_data=testing(data_split)
+
+#modelling
+#Base logistic Regression
+lr_base=glm(stroke~.,data=train_data,family="binomial")
+
+library(broom)
+
+tidy(lr_base)
+
+y_hat=predict(lr_base,test_data,type="response")
+y_hat=as.factor(ifelse(y_hat>0.3,1,0))
+
+#library(caret)
+cm=confusionMatrix(data=y_hat,reference=test_data$stroke)
+cm
+
+#Logistic Regression with class weights
+n0=length(train_data$stroke[train_data$stroke==0])
+n1=length(train_data$stroke)-n0
+
+w0=n1/(n0+n1)
+w1=n0/(n0+n1)
+
+lr_weighted=glm(stroke~.,data=data,family="binomial",weights = ifelse(data$stroke==1,w1,w0))
+tidy(lr_weighted)
+y_hat=predict(lr_weighted,test_data,type="response")
+y_hat=as.factor(ifelse(y_hat>0.6,1,0))
+cm=confusionMatrix(data=y_hat,reference=test_data$stroke)
+cm
+
+#drop or fix work type
+
+#EDA
